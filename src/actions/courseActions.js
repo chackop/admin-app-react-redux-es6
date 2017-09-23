9fb1822deead837432052c74f3d1d6f6e3b4bf65
Chacko <chackop@gmail.com>
2017-09-24 00:52:15 +0100

    load courses

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,18 @@
+import CourseApi from '../api/mockCourseApi';
+import * as types from './actionTypes';
+// import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
+export function loadCoursesSuccess(courses) {
+  return {type: types.LOAD_COURSES_SUCCESS, courses};
+}
 
-export function createCourse(course) {
-  return { type: 'CREATE_COURSES', course};
+export function loadCourses() {
+  return function (dispatch) {
+    // dispatch(beginAjaxCall());
+    return CourseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
+import {loadCourses} from './actions/courseActions';
 import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+
+// Dispatch actions to load initial state.
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
